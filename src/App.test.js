b0fa9a0afa5e706c1637d47bdd46b0a59f593c32
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-07-10 22:09:15 +1000

    Updated Testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
+import { Navbar } from './Components/Layout/Navbar/Navbar';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('should render Navbar', () => {
+  const wrapper = shallow(<App />);
+  const navbar = wrapper.find(Navbar);
+
+  expect(navbar.exists()).toBe(true);
 });
