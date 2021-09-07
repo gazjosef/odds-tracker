a4471f92fd303e3518c36b5281c704b544b063a4
Gareth Hind <garethjhind@gmail.com>
2021-09-07 21:36:41 +1000

    Updated

---
+++ src/components/Event.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Event() {
+  return (
+    <table className="table">
+      <tr>
+        <th>Wed 09 Jun</th>
+        <th>Line</th>
+        <th>Total</th>
+        <th>Win</th>
+      </tr>
+      <tr>
+        <td>ATL Hawks</td>
+        <td>+6.0</td>
+        <td>O225.0</td>
+        <td>$3.10</td>
+      </tr>
+      <tr>
+        <td>PHI 76ers</td>
+        <td>-6.0</td>
+        <td>U225.0</td>
+        <td>$1.40</td>
+      </tr>
+    </table>
+  );
+}
