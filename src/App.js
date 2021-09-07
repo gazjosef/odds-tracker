2c3173367040349bf44acf85aad4a6f8ce3b12ba
Gareth Hind <garethjhind@gmail.com>
2021-09-07 16:27:27 +1000

    Updated Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import "./App.scss";
 
+import Events from "../src/components/Events";
+
 function App() {
   return (
-    <div className="container">
-      <h1>Odds Tracker</h1>
-    </div>
+    <>
+      <header className="header">
+        <div className="header__title">
+          <h1>Odds Tracker</h1>
+        </div>
+      </header>
+      <section className="section-home">
+        <aside className="sidebar-search">
+          <div className="sidebar-search__box">search-box</div>
+          <div className="sidebar-search__league">league</div>
+        </aside>
+        <main className="sport-odds">
+          <div className="sport-odds__title">
+            <h1>Sport</h1>
+          </div>
+          <div className="sport-odds__league"></div>
+          <div className="sport-odds__events">
+            <Events />
+          </div>
+        </main>
+      </section>
+    </>
   );
 }
 
