93835fa465219ba2138c53784957ac3a87d51413
Gareth Hind <garethjhind@gmail.com>
2021-10-04 21:48:03 +1100

    Updated Table

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,24 +3,28 @@ import React from "react";
 export default function Event() {
   return (
     <table className="table">
-      <tr>
-        <th>Wed 09 Jun</th>
-        <th>Line</th>
-        <th>Total</th>
-        <th>Win</th>
-      </tr>
-      <tr>
-        <td>ATL Hawks</td>
-        <td>+6.0</td>
-        <td>O225.0</td>
-        <td>$3.10</td>
-      </tr>
-      <tr>
-        <td>PHI 76ers</td>
-        <td>-6.0</td>
-        <td>U225.0</td>
-        <td>$1.40</td>
-      </tr>
+      <thead>
+        <tr>
+          <th>Wed 09 Jun</th>
+          <th>Line</th>
+          <th>Total</th>
+          <th>Win</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>ATL Hawks</td>
+          <td>+6.0</td>
+          <td>O225.0</td>
+          <td>$3.10</td>
+        </tr>
+        <tr>
+          <td>PHI 76ers</td>
+          <td>-6.0</td>
+          <td>U225.0</td>
+          <td>$1.40</td>
+        </tr>
+      </tbody>
     </table>
   );
 }
