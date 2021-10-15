3bfa302dbc6f4b0b4b5fdb26c82521000c51436a
Gareth Hind <garethjhind@gmail.com>
2021-10-15 15:52:33 +1100

    Updated Code

--- src/App.js
+++ src/App.js
@@ -19,51 +19,98 @@ function App() {
   // const [league, selectLeague] = useState([]);
   // const [market, selectMarket] = useState("--Select Market--");
 
+  // Get Data
   useEffect(() => {
-    const getOdds = async (e) => {
-      // const api_call = await fetch();
-      // `https://api.the-odds-api.com/v3/sports?apiKey=${APIkey}`
-
-      // const apiData = await api_call.json();
-      console.log("Get Data", jsonData);
-
+    const getOdds = () => {
       setData(jsonData);
-
-      let allEvents = [];
-      let allSports = [];
-
-      jsonData.forEach((event) => {
-        allEvents.push(event.group);
-      });
-
-      console.log("Get Events", allEvents);
-
-      allEvents.forEach((name) => {
-        if (allSports.indexOf(name) === -1) {
-          allSports.push(name);
-          setSports((prevState) => [...prevState, name]);
-        }
-      });
-
-      selectSport(sports[0]);
-
-      // jsonData.forEach((event) => {
-      //   if (event.group === sport) {
-      //     // allLeagues.push(event.title);
-      //     setLeagues((prevState) => [...prevState, event.title]);
-      //   }
-      // });
-      // console.log("Check Sports", sports);
-
-      // console.log("Get Leagues", allLeagues);
-
-      // setLeagues(allLeagues);
     };
     getOdds();
-  }, []);
+    console.log("useEffect");
+  });
 
-  console.log("1st/3rd | Check Sports", sports);
-  console.log("1st/3rd | Check Sport", sport);
+  console.log("1st/3rd", data);
+
+  // Set Sports
+  if (data !== undefined && sports.length === 0) {
+    const allSports = data.map((event) => event.group);
+
+    const uniqueSports = [...new Set(allSports)];
+
+    setSports(uniqueSports);
+    console.log("sports has been set");
+    selectSport(uniqueSports[0]);
+    // console.log("First Event", uniqueSports[0]);
+    // setSports()
+  }
+
+  console.log("sports", sports);
+
+  // Set Leagues
+  if (data !== undefined && leagues.length === 0) {
+    data.map((event) => {
+      if (event.group === sport) {
+        setLeagues((prevState) => [...prevState, event.title]);
+      }
+    });
+  }
+  console.log("Leagues", leagues);
+
+  //     // jsonData.forEach((event) => {
+  //     //   if (event.group === sport) {
+  //     //     // allLeagues.push(event.title);
+  //     //     setLeagues((prevState) => [...prevState, event.title]);
+  //     //   }
+  //     // });
+  //     // console.log("Check Sports", sports);
+
+  //     // console.log("Get Leagues", allLeagues);
+
+  // * OLD USEEFFECT()
+  // useEffect(() => {
+  //   const getOdds = async (e) => {
+  //     // const api_call = await fetch();
+  //     // `https://api.the-odds-api.com/v3/sports?apiKey=${APIkey}`
+
+  //     // const apiData = await api_call.json();
+  //     console.log("Get Data", jsonData);
+
+  //     setData(jsonData);
+
+  //     let allEvents = [];
+  //     let allSports = [];
+
+  //     jsonData.forEach((event) => {
+  //       allEvents.push(event.group);
+  //     });
+
+  //     console.log("Get Events", allEvents);
+
+  //     allEvents.forEach((name) => {
+  //       if (allSports.indexOf(name) === -1) {
+  //         allSports.push(name);
+  //         setSports((prevState) => [...prevState, name]);
+  //       }
+  //     });
+
+  //     selectSport(sports[0]);
+
+  //     // jsonData.forEach((event) => {
+  //     //   if (event.group === sport) {
+  //     //     // allLeagues.push(event.title);
+  //     //     setLeagues((prevState) => [...prevState, event.title]);
+  //     //   }
+  //     // });
+  //     // console.log("Check Sports", sports);
+
+  //     // console.log("Get Leagues", allLeagues);
+
+  //     // setLeagues(allLeagues);
+  //   };
+  //   getOdds();
+  // }, []);
+
+  // console.log("1st/3rd | Check Sports", sports);
+  // console.log("1st/3rd | Check Sport", sport);
 
   // data.forEach((event) => {
   //   // console.log(event.group);
@@ -72,17 +119,15 @@ function App() {
   //     setLeagues((prevState) => [...prevState, event.title]);
   //   }
   // });
-  console.log("1st/3rd | Check Leagues", leagues);
+  // console.log("1st/3rd | Check Leagues", leagues);
 
-  // const displaySports = sports.map((sport, index) => (
-  //   <option key={index}>{sport}</option>
-  // ));
+  const displaySports = sports.map((sport, index) => (
+    <option key={index}>{sport}</option>
+  ));
 
-  // const displayLeagues = () => {
-  //   if (league === undefined) {
-  //     <option>-- Select League --</option>;
-  //   }
-  // };
+  const displayLeagues = leagues.map((league, index) => (
+    <option key={index}>{league}</option>
+  ));
 
   const handleChange = (e) => {
     selectSport(e.target.value);
@@ -113,8 +158,8 @@ function App() {
           </div>
         </main>
         <Search
-          // displaySports={displaySports}
-          // displayLeagues={displayLeagues}
+          displaySports={displaySports}
+          displayLeagues={displayLeagues}
           findOdds={findOdds}
           sport={sport}
           // league={league}
