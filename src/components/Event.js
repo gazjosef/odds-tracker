1145864d9204f31537fa66b55399d4f38e16156b
Gareth Hind <garethjhind@gmail.com>
2021-10-18 15:31:52 +1100

    Updated

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,30 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Event({ h2h }) {
+export default function Event({ h2h, spreads, totals }) {
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Wed 09 Jun</th>
-          <th>Line</th>
-          <th>Total</th>
-          <th>Win</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>ATL Hawks</td>
-          <td>+6.0</td>
-          <td>O225.0</td>
-          <td>$3.10</td>
-        </tr>
-        <tr>
-          <td>PHI 76ers</td>
-          <td>-6.0</td>
-          <td>U225.0</td>
-          <td>$1.40</td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      {h2h &&
+        h2h.map((event, index) => {
+          return (
+            <>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Wed 09 Jun</th>
+                    <th>Line</th>
+                    <th>Total</th>
+                    <th>Win</th>
+                  </tr>
+                </thead>
+                <tbody key={index}>
+                  <tr>
+                    <td>{event.away_team}</td>
+                    <td>+6.0</td>
+                    <td>O225.0</td>
+                    <td>$3.10</td>
+                  </tr>
+                  <tr>
+                    <td>{event.home_team}</td>
+                    <td>-6.0</td>
+                    <td>U225.0</td>
+                    <td>$1.40</td>
+                  </tr>
+                </tbody>
+              </table>
+              {displaySites(h2h)}
+            </>
+          );
+        })}
+    </>
+    // <table className="table">
+    //   <thead>
+    //     <tr>
+    //       <th>Wed 09 Jun</th>
+    //       <th>Line</th>
+    //       <th>Total</th>
+    //       <th>Win</th>
+    //     </tr>
+    //   </thead>
+    //   <tbody>
+    //     <tr>
+    //       <td>ATL Hawks</td>
+    //       <td>+6.0</td>
+    //       <td>O225.0</td>
+    //       <td>$3.10</td>
+    //     </tr>
+    //     <tr>
+    //       <td>PHI 76ers</td>
+    //       <td>-6.0</td>
+    //       <td>U225.0</td>
+    //       <td>$1.40</td>
+    //     </tr>
+    //   </tbody>
+    // </table>
   );
 }
