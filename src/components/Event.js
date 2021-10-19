1472b95b095333a343c9bb3b43672b9670fe73dd
Gareth Hind <garethjhind@gmail.com>
2021-10-19 14:18:06 +1100

    Updated

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,63 +1,40 @@
 import React from "react";
 
 export default function Event({ h2h, spreads, totals }) {
+  console.log("h2h", h2h);
   return (
     <>
       {h2h &&
-        h2h.map((event, index) => {
+        h2h.map((event, key) => {
           return (
-            <>
-              <table className="table" key={index}>
-                <thead>
-                  <tr key={index}>
-                    <th>Wed 09 Jun</th>
-                    <th>Line</th>
-                    <th>Total</th>
-                    <th>Win</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>{event.away_team}</td>
-                    <td>+6.0</td>
-                    <td>O225.0</td>
-                    <td>$3.10</td>
-                  </tr>
-                  <tr>
-                    <td>{event.home_team}</td>
-                    <td>-6.0</td>
-                    <td>U225.0</td>
-                    <td>$1.40</td>
-                  </tr>
-                </tbody>
-              </table>
-            </>
+            <table className="table" key={key}>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>{event.sport_title}</th>
+                  <th>Line</th>
+                  <th>Total</th>
+                  <th>Win</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td rowSpan="2">Date</td>
+                  <td>{event.away_team}</td>
+                  <td>+6.0</td>
+                  <td>O225.0</td>
+                  <td>$3.10</td>
+                </tr>
+                <tr>
+                  <td>{event.home_team}</td>
+                  <td>-6.0</td>
+                  <td>U225.0</td>
+                  <td>$1.40</td>
+                </tr>
+              </tbody>
+            </table>
           );
         })}
     </>
-    // <table className="table">
-    //   <thead>
-    //     <tr>
-    //       <th>Wed 09 Jun</th>
-    //       <th>Line</th>
-    //       <th>Total</th>
-    //       <th>Win</th>
-    //     </tr>
-    //   </thead>
-    //   <tbody>
-    //     <tr>
-    //       <td>ATL Hawks</td>
-    //       <td>+6.0</td>
-    //       <td>O225.0</td>
-    //       <td>$3.10</td>
-    //     </tr>
-    //     <tr>
-    //       <td>PHI 76ers</td>
-    //       <td>-6.0</td>
-    //       <td>U225.0</td>
-    //       <td>$1.40</td>
-    //     </tr>
-    //   </tbody>
-    // </table>
   );
 }
